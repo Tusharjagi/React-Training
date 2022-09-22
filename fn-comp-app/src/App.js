3531e7ef825ad3377ff0c9c8f663d1fe334dd9ac
tusharjagi <tusharjagi@gmail.com>
2022-09-23 01:18:47 +0530

    down compelete

--- fn-comp-app/src/App.js
+++ fn-comp-app/src/App.js
@@ -1,11 +1,29 @@
 import Redered from "./components/renderedData";
 import SubmitData from "./components/submitData";
+import { useState } from "react";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [textArray, setTextArray] = useState([]);
+  const [showText, setShowText] = useState("there is no items ......")
+
+  const handleOnChange = (event) => {
+    setInputText(event.target.value);
+    // console.log("on change")
+  };
+
+  const onClickHandler = () => {
+    let newArray = [...textArray, inputText];
+    setTextArray(newArray);
+    setInputText("");
+    console.log(newArray);
+    // console.log("Clicked")
+  };
+
   return (
     <>
       <Redered />
-      <SubmitData/>
+      <SubmitData clickHandle={onClickHandler} changeHandle={handleOnChange} Text={inputText}/>
     </>
   );
 }
--- fn-comp-app/src/components/submitData.js
+++ fn-comp-app/src/components/submitData.js
@@ -1,28 +1,37 @@
 import Button from "./submitComponents/button";
 import Input from "./submitComponents/input";
-import { useState } from "react";
+// import { useState } from "react";
 
-function SubmitData() {
-    const [inputText, setInputText] = useState("")
-    const [textArray, setTextArray] = useState([])
-    let newArray = [];
+function SubmitData(props) {
+    // const [inputText, setInputText] = useState("")
+    // const [textArray, setTextArray] = useState([])
 
-    const handleOnChange = (event) => {
-        setInputText(event.target.value)
-        console.log(event.target.value)
-    }
+    // const handleOnChange = (event) => {
+    //     setInputText(event.target.value)
+    //     // console.log(event.target.value)
+    // }
 
-    const onClickHandler = () => {
-        // setTextArray(textArray => [...Array,textArray]);
-        // setTextArray(prev => ([...prev,setInputText]))
-        // newArray.push(inputText);
-        console.log("Clicked")
-    }
+    // const onClickHandler = () => {
+    //     const newArray = [...textArray,inputText]
+    //     setTextArray(newArray);
+    //     setInputText("")
+    //     // setTextArray("")
+    //     // setTextArray(textArray => [...Array,textArray]);
+    //     // setTextArray(prev => ([...prev,setInputText]))
+    //     // newArray.push(inputText);
+    //     // setTextArray(prev => ([...prev,setTextArray]))
+    //     // console.log();
+    //     console.log(newArray);
+    //     // console.log(news)
+    //     console.log("Clicked")
+    // }
 
     return(
-        <div className="main">
-         <Input value={inputText} onChanges={handleOnChange}/>
-         <Button clickHandler={onClickHandler}/>
+        <div className="main"> 
+         <Input value={props.Text} onChanges={props.changeHandle}/>
+         <Button clickHandler={props.clickHandle}/>
+         {/* <Input value={inputText} onChanges={handleOnChange} />
+         <Button clickHandler={onClickHandler}/> */}
         </div>
     )
 }
