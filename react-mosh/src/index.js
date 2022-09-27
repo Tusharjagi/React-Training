9f3b833123fd61c972fd886da502bcac151af8fa
tusharjagi <tusharjagi@gmail.com>
2022-09-27 11:30:12 +0530

    compelete mosh class components

--- react-mosh/src/index.js
+++ react-mosh/src/index.js
@@ -6,6 +6,7 @@ const element = <h1>Hello world</h1>
 console.log(element);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // ReactDOM.render(element, document.getElementById('root'));
+
 root.render(
     <React.StrictMode>
         {element}
