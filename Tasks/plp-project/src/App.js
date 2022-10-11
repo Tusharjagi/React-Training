37db1188a29b1b52b5cc46cff8f745fa781f016c
tusharjagi <tusharjagi@gmail.com>
2022-10-11 22:24:55 +0530

    lazy loading

--- Tasks/plp-project/src/App.js
+++ Tasks/plp-project/src/App.js
@@ -1,18 +1,20 @@
-import TopNavBar from "./components/plp/navBar";
-import CardComponents from "./components/plp/Cards";
-import ItemPage from "./components/pdp/ItemPages";
+import React, { Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// const CardComponents = React.lazy(() => import('./components/plp/Cards'));
+const CardComponents = React.lazy(() => import("./components/plp/Cards"));
+const ItemPage = React.lazy(() => import("./components/pdp/ItemPages"));
+const TopNavBar = React.lazy(() => import("./components/plp/navBar"));
 
 function App() {
   return (
     <BrowserRouter>
+      <Suspense>
       <TopNavBar />
-      <Routes>
-        <Route path="/" element={<div>Home page</div>} />
-        <Route path="/products" element={<CardComponents />} />
-        <Route path="/products/:itemId" element={<ItemPage />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<div>Home page</div>} />
+          <Route path="/products" element={<CardComponents />} />
+          <Route path="/products/:itemId" element={<ItemPage />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
