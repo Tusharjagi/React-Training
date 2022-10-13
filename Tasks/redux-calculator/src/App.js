dc5c8c3c2331061380eb61cdb38ed1cb51010561
tusharjagi <tusharjagi@gmail.com>
2022-10-13 12:00:15 +0530

    font size change

--- Tasks/redux-calculator/src/App.js
+++ Tasks/redux-calculator/src/App.js
@@ -1,39 +1,40 @@
-import './App.css';
-import Calculate from './Components/Calculate';
-import Display from './Components/Display';
-import Header from './Components/Header';
-import Eight from './Components/Numbers/Eight';
-import Five from './Components/Numbers/Five';
-import Four from './Components/Numbers/Four';
-import Nine from './Components/Numbers/Nine';
-import One from './Components/Numbers/One';
-import Seven from './Components/Numbers/Seven';
-import Six from './Components/Numbers/Six';
-import Three from './Components/Numbers/Three';
-import Two from './Components/Numbers/Two';
-import Zero from './Components/Numbers/Zero';
-import Minus from './Components/Operators/Minus';
-import Plus from './Components/Operators/Plus';
+import "./App.css";
+import Calculate from "./Components/Calculate";
+import Display from "./Components/Display";
+import Header from "./Components/Header";
+import Eight from "./Components/Numbers/Eight";
+import Five from "./Components/Numbers/Five";
+import Four from "./Components/Numbers/Four";
+import Nine from "./Components/Numbers/Nine";
+import One from "./Components/Numbers/One";
+import Seven from "./Components/Numbers/Seven";
+import Six from "./Components/Numbers/Six";
+import Three from "./Components/Numbers/Three";
+import Two from "./Components/Numbers/Two";
+import Zero from "./Components/Numbers/Zero";
+import Minus from "./Components/Operators/Minus";
+import Plus from "./Components/Operators/Plus";
 
 function App() {
-  return (<>
-      <Header/>
-    <div className="App">
-      <Display/>
-      <One/>
-      <Two/>
-      <Three/>
-      <Four/>
-      <Five/>
-      <Six/>
-      <Seven/>
-      <Eight/>
-      <Nine/>
-      <Minus/>
-      <Zero/>
-      <Plus/>
-    </div>
-      <Calculate/>
+  return (
+    <>
+      <Header />
+      <div className="App">
+        <Display />
+        <One />
+        <Two />
+        <Three />
+        <Four />
+        <Five />
+        <Six />
+        <Seven />
+        <Eight />
+        <Nine />
+        <Minus />
+        <Zero />
+        <Plus />
+      </div>
+      <Calculate />
     </>
   );
 }
