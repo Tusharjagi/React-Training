2d7648dfc82e6b98d2d4016610dcb75be2ab5a42
tusharjagi <tusharjagi@gmail.com>
2022-10-21 15:04:12 +0530

    counter

--- Practise/react-mosh-counter/src/App.js
+++ Practise/react-mosh-counter/src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
 
   constructor(props){
     super(props);
-    console.log("App-Contructor");
+    console.log("App-Constructor");
     // this.state = this.props.somethings;
     
   }
