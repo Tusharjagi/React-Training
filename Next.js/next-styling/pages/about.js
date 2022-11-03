9bab23af7a3420ea8a8bfb91e4b35c3fdd2e35f3
tusharjagi <tusharjagi@gmail.com>
2022-11-03 14:27:44 +0530

    styling in next JS

---
+++ Next.js/next-styling/pages/about.js
@@ -0,0 +1,11 @@
+import styles from '../styles/About.module.scss'
+
+const About = () => {
+  return (
+    <>
+      <div className={styles.highlightscss}>About Page</div>
+      <button className="btn btn-success">Primary</button>
+    </>
+  );
+};
+export default About;
