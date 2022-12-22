76601465d1428e79cc6cbe42ca5cc4deff8b3d4c
Tusharjagi <tusharjagi@gmail.com>
2022-12-22 11:47:51 +0530

    middleware

--- Middleware/saga/src/redux/productReducer.js
+++ Middleware/saga/src/redux/productReducer.js
@@ -1,11 +1,11 @@
 import { PRODUCT_LIST, SET_PRODUCT_LIST } from "./constant";
 
-// export const productData = (data = [], action) => {
-//   switch (action.type) {
-//     case SET_PRODUCT_LIST:
-//       console.log("SET_PRODUCT_LIST",action);
-//       return [...action.data];
-//     default:
-//       return data;
-//   }
-// };
+export const productData = (data = [], action) => {
+  switch (action.type) {
+    case SET_PRODUCT_LIST:
+      console.log("SET_PRODUCT_LIST", action);
+      return [...action.data];
+    default:
+      return data;
+  }
+};
